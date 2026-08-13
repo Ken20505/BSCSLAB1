374935ffc9fb2e55d7f37f3eedabe6e690d7fd55
French Kent Calibo <s2201251@usls.edu.ph>
2026-08-13 22:53:41 +0800

    lab2 pages wahooooo

--- lab2/page1.php
+++ lab2/page1.php
@@ -8,9 +8,9 @@
     </head>
     <body>
         <h1><?php echo $header; ?></h1>
-        <p>Hi, I am French Kent Calibo, a Computer Science student. Welcome to my personal portfolio website created for my PHP lab assignment!</p>
+        <p>Hi, I am French Kent Calibo, a Computer Science student. Welcome to my portfolio page!</p>
 
-        <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t39.30808-6/758854539_414509850578888_6725227845339243769_n.jpg" alt="French Kent Calibo" width="200"><br><br>
+        <img src="https://scontent.fceb6-4.fna.fbcdn.net/v/t39.30808-6/770847082_4157811824517643_678660992820376268_n.jpg?stp=dst-jpg_tt6&cstp=mx1284x1284&ctp=s1284x1284&_nc_cat=107&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGnirylobjwqxXv3WHQ2ZMKeZW9mPmFBaV5lb2Y-YUFpe_-n7KkKn5cQ9IUXNNlnKUikZ8G_H0cekmZMKJ-kzW_&_nc_ohc=GKXhlsi5HEsQ7kNvwFyxTok&_nc_oc=AdrGdkMvbB3XiDnqil0mimRRWgs_CL30bPFPelGMmCTRMTBTwh7D6LMmtzCrB8wECk5-d1EBbnpfK2SHl0h2nEj5&_nc_zt=23&_nc_ht=scontent.fceb6-4.fna&_nc_gid=8dPEZFq3GtWLJWKaOPqx1Q&_nc_ss=7b2a8&oh=00_AQF8xHg3kSIg6UygEazSt0IU5FRNhXKdZfVZKrJ280qBsQ&oe=6A83B4D5" alt="French Kent Calibo" width="200"><br><br>
 
         <h3>Navigation:</h3>
         <a href="page2.php">Go to Projects (Page 2)</a> |
