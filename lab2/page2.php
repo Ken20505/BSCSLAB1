945f8665dbe2a8e40de2a9b751d8a99a70ba2eb9
French Kent Calibo <s2201251@usls.edu.ph>
2026-08-13 23:05:13 +0800

    lab2 pages wahooooo

--- lab2/page2.php
+++ lab2/page2.php
@@ -8,9 +8,9 @@
     </head>
     <body>
         <h1><?php echo $header; ?></h1>
-        <p>This page showcases the programming projects I have worked on during my Computer Science daysg.</p>
+        <p>This page showcases the programming projects I have worked on during my Computer Science days.</p>
 
-        <img src="https://careerkarma.com/blog/wp-content/uploads/2021/11/top-coding-projects.png" alt="Projects Work" width="200"><br><br>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOdcOFoHfAvy_w0tpNJEKNWFrHBIdfDHg2yA1cP8YYnq1Oq687BcEeHRh&s=10" alt="Projects Image" width="200"><br><br>
 
         <h3>Navigation:</h3>
         <a href="page1.php">Go to Home (Page 1)</a> |
